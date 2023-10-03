56bb1ba7c6e1b21872391c13cf5f4ced35d9e07f
Mateo Torres <torresmateo@gmail.com>
2023-10-03 19:33:41 -0400

    implemented basic progress checking

--- yulab/calculate_progress.py
+++ yulab/calculate_progress.py
@@ -1,4 +1,35 @@
 from pathlib import Path
+import logging
+
+logger = logging.getLogger('calculate_progress')
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    "%Y-%m-%d %H:%M:%S")
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+def run(outdir: Path, pairs: Path) -> None:
+    assert outdir.exists(), "output directory does not exist"
+    assert pairs.is_file(), "PDB pairs is not a file"
+
+    # for now, we simply care about the number of items.
+    unique_pairs = set()
+    with pairs.open() as p:
+        for line in p:
+            unique_pairs.add(line.strip())
+
+    tot = len(unique_pairs)
+
+    finished = 0
+    predir = outdir / "preds" / "struct"
+    for _ in predir.glob("*.lig.gz"):
+        finished += 1
+
+    logger.info(f"Progress = {finished}/{tot} ({finished/tot*100.0:.2f}%)")
 
 
 if __name__ == "__main__":
@@ -6,8 +37,6 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description="Reads Yulab's PDBs and prepares the files for BIPSPI+")
-    parser.add_argument("-i", "--input-dir", required=True,
-                        help="A directory containing PDF files")
     parser.add_argument("-o", "--output-dir", required=True,
                         help="A path to write the renamed PDB files")
     parser.add_argument("-p", "--pdb-pairs", required=True,
